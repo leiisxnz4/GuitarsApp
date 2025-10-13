6e5c734ef65fabe848c6044e61b2196604db44bd
leiisxnz4 <leisanchez283@gmail.com>
2025-10-13 11:21:22 -0600

    01 Load guitars

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/*
 import { db } from './guitarras.js';
 
 // Iterar arrays
@@ -21,3 +22,70 @@ db.forEach((guitar) =>
     {console.log(guitar.nombre)
     container.appendChild(createCard(guitar.nombre))
 });
+*/
+
+import { db } from './guitarras.js';
+
+const divContainer = document.querySelector('main div');
+// const carrito = []
+
+const createCard = (guitar) => {
+    const div = document.createElement('div')
+    div.className = 'col-md-6 col-lg-4 my-4 row align-items-center'
+    const html = ` <div class="col-4">
+                    <img class="img-fluid" src="./img/${guitar.imagen}.jpg" alt="imagen guitarra">
+                </div>
+                <div class="col-8">
+                    <h3 class="text-black fs-4 fw-bold text-uppercase">${guitar.nombre}</h3>
+                    <p>${guitar.descripcion}</p>
+                    <p class="fw-black text-primary fs-3">$${guitar.precio}</p>
+                    <button 
+                        type="button"
+                        class="btn btn-dark w-100 "
+                    >Agregar al Carrito</button>
+                </div>`
+    div.innerHTML = html;
+    return div
+}
+
+/*
+const createDiv = (guitar) => {
+    const div = document.createElement('div');
+    div.className = 'col-md-6 col-lg-4 my-4 row align-items-center'
+    const html = `<div class="col-4">
+                    <img class="img-fluid" src="./img/${guitar.imagen}.jpg" alt="imagen guitarra">
+                </div>
+                <div class="col-8">
+                    <h3 class="text-black fs-4 fw-bold text-uppercase">${ guitar.nombre }</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
+                    <p class="fw-black text-primary fs-3">$${ guitar.precio}</p>
+                    <button 
+                        type="button"
+                        class="btn btn-dark w-100 "
+                    >Agregar al Carrito</button>
+                </div>`
+    div.innerHTML = html;
+    return div;
+}
+*/
+
+//utilizando interadores
+db.forEach( guitar => {
+    divContainer.appendChild(createCard(guitar) );
+})
+
+/*
+const cardClicked = (e) => {
+    if (e.target.classList.contains('btn')){
+    // console.log('Le diste al botón', e.target.getAttribute('data'))}
+    const idGuitar = e.target.getAttribute('data-id')
+    const indexdb= db.findIndex(guitar => guitar.id === Number (idGuitar))
+    carrito.push({...db[indexdb], 
+        cantidad: 1
+    })
+    console.log(carrito)
+    }
+}
+
+divContainer.addEventListener('Click', cardClicked)
+*/
